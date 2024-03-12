bebfef1f72f60c11f5b1de5d613a4f337a6e0a8f
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-03-12 16:49:16 -0300

    Exercício 3 - v4 feito!

---
+++ src/ADO1/Exercicios/Exercicio03_v4/Opcoes.java
@@ -0,0 +1,6 @@
+package Exercicio03_v4;
+
+public class Opcoes {
+    public int codCurso;
+    public int codBolsa;
+}
---
+++ src/ADO1/Exercicios/Exercicio03_v4/Resultado.java
@@ -0,0 +1,9 @@
+package Exercicio03_v4;
+
+public class Resultado {
+    public double mensalidade;
+    public String curso;
+    public String programaBolsa;
+    public double desconto;
+
+}
