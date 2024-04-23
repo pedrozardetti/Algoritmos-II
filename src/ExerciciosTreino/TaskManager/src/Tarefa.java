b92a5008bf892944db2b2d40a42b11a84696cda4
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-23 17:47:27 -0300

    Classe para ser o modelo do objeto tarefa

--- src/ExerciciosTreino/TaskManager/src/Tarefa.java
+++ src/ExerciciosTreino/TaskManager/src/Tarefa.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Tarefa {
+import java.time.LocalDateTime;
+
+public class Tarefa {
+
+    private int id;
+    private String nome;
+
+    private String descricao;
+
+    private LocalDateTime createdAt;
+
+    public Tarefa(int id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.createdAt = LocalDateTime.now();
+
+
+    }
+
+    public Tarefa() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
