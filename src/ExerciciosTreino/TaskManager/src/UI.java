a5a7952aadc73ce82234990a5ad1002d035c548f
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-23 17:47:51 -0300

    Classe com a responsabilidade de interação com usuário

--- src/ExerciciosTreino/TaskManager/src/UI.java
+++ src/ExerciciosTreino/TaskManager/src/UI.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class UI {
+import java.util.Scanner;
+
+public class UI {
+    public Scanner sc = new Scanner(System.in);
+
+    public int menu() {
+        System.out.println("1. Nova Tarefa");
+        System.out.println("2. Listar Tarefas");
+        System.out.println("3. Remover Tarefa");
+        System.out.println("4. Marcar como resolvido");
+        System.out.println("0. Sair");
+        return sc.nextInt();
+    }
+
+
+    public Tarefa novaTarefa() {
+        System.out.println("Informe o ID da nova tarefa");
+        int id = sc.nextInt();
+        System.out.println("Informe a nova tarefa");
+        String novaTarefa = sc.nextLine();
+        sc.next();
+        System.out.println("Informe a descrição da nova tarefa");
+        String novaDesc = sc.nextLine();
+        Tarefa t = new Tarefa(id, novaTarefa, novaDesc);
+        return t;
+    }
+
+    public int pedirTarefa() {
+        System.out.println("Qual o ID da tarefa que deseja remover?");
+        int id = sc.nextInt();
+        return id;
+    }
+
 }
+
