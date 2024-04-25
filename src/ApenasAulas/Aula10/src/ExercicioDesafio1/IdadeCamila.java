61785495809bec7e4ea01bd18b34ecfe157020a2
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-25 16:29:20 -0300

    Exercício da idade da Camila

--- src/ApenasAulas/Aula10/src/ExercicioDesafio1/IdadeCamila.java
+++ src/ApenasAulas/Aula10/src/ExercicioDesafio1/IdadeCamila.java
@@ -1,13 +1,23 @@
 package ExercicioDesafio1;
 
+import java.util.Arrays;
+
 //https://olimpiada.ic.unicamp.br/pratique/pj/2021/f1/idade/
+
 public class IdadeCamila {
-    
+
     IdadesIrmas idadeIrmas = new IdadesIrmas();
 
+    public int descobrirIdade(int idade1, int idade2, int idade3) {
+        // Armazenamos as idades em um array
+        int[] idades = {idade1, idade2, idade3};
 
-    public IdadesIrmas descobrirIdade(int idade1, int idade2, int idade3) {
+        // Ordenamos as idades em ordem crescente
+        Arrays.sort(idades);
 
+        // A idade do meio é a idade de Camila
+        int idadeDeCamila = idades[1];
+        
+        return idadeDeCamila;
     }
-
 }
--- src/ApenasAulas/Aula10/src/ExercicioDesafio1/Menu.java
+++ src/ApenasAulas/Aula10/src/ExercicioDesafio1/Menu.java
@@ -6,7 +6,20 @@ public class Menu {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        IdadeCamila idadeCamila = new IdadeCamila();
+
+        System.out.println("Informe a idade de Cibele:");
+        int idadeCibele = sc.nextInt();
+
+        System.out.println("Informe a idade de Camila:");
+        int idadeC = sc.nextInt();
+
+        System.out.println("Informe a idade de Celeste:");
+        int idadeCeleste = sc.nextInt();
+
+        int idadeDeCamila = idadeCamila.descobrirIdade(idadeCibele, idadeC, idadeCeleste);
+
+        System.out.println("A idade de Camila é: " + idadeDeCamila);
         
     }
 
