fa86c67b1dead61f7e3f630578fbebec7e8bb2dc
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-30 15:41:37 -0300

    Adição das funcionalidades de adicionar nova reserva, chamar próximo da fila e listar reservas

--- src/ApenasAulas/Aula9/src/Exercicio1/ControleReserva.java
+++ src/ApenasAulas/Aula9/src/Exercicio1/ControleReserva.java
@@ -8,25 +8,24 @@ public class ControleReserva {
   private Queue<Reserva> reservas = new ArrayDeque<>();
 
   public void novaReserva(Reserva reserva) {
-
+    reservas.offer(reserva); // Adiciona a reserva à fila
+    System.out.println("Reserva feita para " + reserva.responsavel + " com " + reserva.lugares + " lugares.");
   }
 
   public void listar() {
+    System.out.println("\n--- Lista de Reservas ---");
     for (Reserva reserva : reservas) {
-      System.out.println(reservas);
-      
+      System.out.println("Responsável: " + reserva.responsavel + ", Lugares: " + reserva.lugares);
     }
   }
 
-  public Reserva proximo() {
-    reservas.peek();
-    return;
+  public void proximo() {
+    if (!reservas.isEmpty()) {
+      Reserva proximaReserva = reservas.poll(); // Remove e retorna a próxima reserva
+      System.out.println("\nChamando próxima reserva para " + proximaReserva.responsavel + " com " + proximaReserva.lugares + " lugares.");
+    } else {
+      System.out.println("\nNão há reservas no momento.");
+    }
   }
-
-
-
-
-
-
-
 }
+
