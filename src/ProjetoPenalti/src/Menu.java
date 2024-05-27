8b56a7aac0afaadceb442afb5ee3eebcd2f7ed5d
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-05-26 21:52:44 -0300

    feat criar classe menu para iniciar o jogo e interagir com  usuário

--- src/ProjetoPenalti/src/Menu.java
+++ src/ProjetoPenalti/src/Menu.java
@@ -1,5 +1,8 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+public class Menu {
+    public static void main(String[] args) {
+        JogoPenalti jogo = new JogoPenalti();
+        jogo.executar();
     }
 }
+
